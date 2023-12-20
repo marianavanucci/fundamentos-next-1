ad63203c0a57a0236097ec8b4bfe1004b9f9d189
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-12-20 17:06:13 -0300

    fixing tailwind setup

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,18 @@
 import styles from '../styles/Home.module.css'
 
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <div>
-        <h1 className="text-3xl font-bold underline">
-    Hello world!djfhdfsdgfdgdfgd
-  </h1>
+      <Head>
+          <link rel="stylesheet" href="/dist/output.css" />
+      </Head>
+      <div>
+        <h1 class="text-3xl font-bold underline">
+        Hello world!
+        </h1>
+      </div>
     </div>
   )
 }
